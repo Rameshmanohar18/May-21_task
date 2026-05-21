695f498dca2f6d1d9e17556e58c52b40e8fff2ea
Rameshmanohar18 <rameshbradmandgl@gmail.com>
2026-05-21 18:26:29 +0530

    Usestae counter modified

--- May-21/vite-project/src/pages/counterpage.jsx
+++ May-21/vite-project/src/pages/counterpage.jsx
@@ -7,20 +7,40 @@ function CounterPage() {
     console.log("Updated:", count);
   }, [count]);
 
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, count),
-    React.createElement(
-      "button",
-      {
-        onClick: () => {
-          setCount((currentCount) => currentCount + 1);
-        },
-      },
-      "Increment"
-    )
-  );
+
+  return (
+    <>
+    <div>
+<h2>Count:{count} </h2>
+<button onClick={()=> setCount(count +1) }>Increment </button>
+<button onClick={()=> setCount(count -1)} disabled={count === 0} >Decrement </button>
+<button onClick={()=> setCount(0) }>Reset </button>
+
+
+
+
+    </div>
+    
+    </>
+
+
+
+
+  )
+  // return React.createElement(
+  //   "div",
+  //   null,
+  //   React.createElement("h1", null, count),
+  //   React.createElement(
+  //     "button",
+  //     {
+  //       onClick: () => {
+  //         setCount((currentCount) => currentCount + 1);
+  //       },
+  //     },
+  //     "Increment"
+  //   )
+  // );
 }
 
 export default CounterPage;
